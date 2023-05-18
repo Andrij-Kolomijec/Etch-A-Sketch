6c99547aba70d88ecd603df78f050e562c567591
Difficins <a.kolomijec@seznam.cz>
2023-05-18 21:05:13 +0200

    Minor adjustements

--- javascript.js
+++ javascript.js
@@ -1,30 +1,39 @@
 // Create the grid
 const div = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-    let divI = document.createElement("div");
-    for (let j = 0; j < 16; j++) {
-        let divJ = document.createElement("div");
-        divJ.setAttribute("class", "inside")
-        divI.appendChild(divJ);
+function generateGrid(number) {
+    for (let i = 0; i < number; i++) {
+        let divI = document.createElement("div");
+        for (let j = 0; j < number; j++) {
+            let divJ = document.createElement("div");
+            divJ.setAttribute("class", "inside")
+            divI.appendChild(divJ);
+        }
+        div.appendChild(divI);
     }
-    div.appendChild(divI);
 }
+
+generateGrid(16);
 
 // Mouse hover event and change color
-div.addEventListener("mouseover", e => changeColor(e.target));
-
-function changeColor(tar) {
-    tar.style.background = "blue";
-}
+div.addEventListener("mouseover", e => e.target.style.background = "blue");
 
 // Button to change the grid
+let height = document.querySelector("div").style.height;
+let width = document.querySelector("div").style.width;
 let squares = 16;
+const inside = document.querySelector(".inside");
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     squares = Number(prompt("Change the number of squares per side (up to 100):", 16));
     while (isNaN(squares) || squares > 100 || squares < 1) {
         squares = Number(prompt("Must be a number from 1 to 100!", 16));
     }
-    div.style.height
+    clearDiv();
+    height = div.style.height = `${480/squares}px`;
+    width = div.style.width = height;
+    generateGrid(squares);
 });
 
+function clearDiv() {
+    document.querySelector("#container").innerHTML = "";
+}
