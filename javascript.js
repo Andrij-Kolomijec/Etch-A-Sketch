5b68a5f9ee5a977c6d60505bd0b77e30619b8d00
Difficins <a.kolomijec@seznam.cz>
2023-05-18 14:33:55 +0200

    Changes color

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
+// Create the grid
 const div = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
     let divI = document.createElement("div");
     for (let j = 0; j < 16; j++) {
         let divJ = document.createElement("div");
-        divJ.setAttribute("id", "inside")
+        divJ.setAttribute("class", "inside")
         divI.appendChild(divJ);
     }
     div.appendChild(divI);
 }
+
+// Create mouse hover event and change color
+div.addEventListener("mouseover", e => changeColor(e.target));
+
+function changeColor(tar) {
+    tar.style.background = "blue";
+}
+
+
