1b11f5d914c0968957af10f901d97bdd6742e64f
Difficins <a.kolomijec@seznam.cz>
2023-05-24 12:44:34 +0200

    Finished the button to change the grid

--- javascript.js
+++ javascript.js
@@ -1,39 +1,40 @@
 // Create the grid
-const div = document.querySelector("#container");
+const container = document.querySelector("#container");
+let squares = 16;
+
 function generateGrid(number) {
+    let boxSize = `${480 / squares}px`;
     for (let i = 0; i < number; i++) {
-        let divI = document.createElement("div");
+        let row = document.createElement("div");
+        row.setAttribute("class", "row");
         for (let j = 0; j < number; j++) {
-            let divJ = document.createElement("div");
-            divJ.setAttribute("class", "inside")
-            divI.appendChild(divJ);
+            let box = document.createElement("div");
+            box.setAttribute("class", "box");
+            box.style.width = boxSize;
+            box.style.height = boxSize;
+            row.appendChild(box);
         }
-        div.appendChild(divI);
+        container.appendChild(row);
     }
 }
 
-generateGrid(16);
+generateGrid(squares);
 
 // Mouse hover event and change color
-div.addEventListener("mouseover", e => e.target.style.background = "blue");
+container.addEventListener("mouseover", e => e.target.style.background = "blue");
 
 // Button to change the grid
-let height = document.querySelector("div").style.height;
-let width = document.querySelector("div").style.width;
-let squares = 16;
-const inside = document.querySelector(".inside");
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-    squares = Number(prompt("Change the number of squares per side (up to 100):", 16));
-    while (isNaN(squares) || squares > 100 || squares < 1) {
-        squares = Number(prompt("Must be a number from 1 to 100!", 16));
+    let newSquares = Number(prompt("Change the number of squares per side (up to 100):", 16));
+    while (isNaN(newSquares) || newSquares > 100 || newSquares < 1) {
+        newSquares = Number(prompt("Must be a number from 1 to 100!", 16));
     }
-    clearDiv();
-    height = div.style.height = `${480/squares}px`;
-    width = div.style.width = height;
+    squares = newSquares;
+    clearGrid();
     generateGrid(squares);
 });
 
-function clearDiv() {
-    document.querySelector("#container").innerHTML = "";
+function clearGrid() {
+    container.innerHTML = "";
 }
