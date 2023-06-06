f810ba48ef66b7c48fbf8a713434ab0ed6aa4876
Difficins <a.kolomijec@seznam.cz>
2023-06-06 15:16:48 +0200

    Added grey scale button

--- javascript.js
+++ javascript.js
@@ -2,10 +2,8 @@
 const container = document.querySelector("#container");
 let squares = 16;
 let isRainbowActive = false;
+let isGreyScaleActive = false;
 let isMouseDown = false;
-
-document.addEventListener("mousedown", () => isMouseDown = true);
-document.addEventListener("mouseup", () => isMouseDown = false);
 
 function generateGrid(number) {
     let boxSize = `${480 / squares}px`;
@@ -18,13 +16,15 @@ function generateGrid(number) {
             box.style.width = boxSize;
             box.style.height = boxSize;
             row.appendChild(box);
-        }
+        };
         container.appendChild(row);
         if (isRainbowActive) rainbowMode();
+        if (isGreyScaleActive) greyScaleMode();
     };
 };
 
 generateGrid(squares);
+
 
 //Change the grid size with slider
 let isSliderActive = false;
@@ -33,9 +33,8 @@ const label = document.querySelector("label");
 slider.addEventListener("mousedown", () => isSliderActive = true);
 slider.addEventListener("mousemove", () => {
     if (isSliderActive) {
-        let newSquares = slider.value;
-        squares = newSquares;
-        clearGrid();
+        squares = slider.value;
+        container.innerHTML = "";
         generateGrid(squares);
         label.textContent = `${squares}x${squares} squares`;
         isSliderActive = false;
@@ -43,13 +42,18 @@ slider.addEventListener("mousemove", () => {
     }
 });
 
-// Mouse hover event and change color
-let color = "black";
+
+// Mouse down + hover, mouse up events
+document.addEventListener("mousedown", () => isMouseDown = true);
+document.addEventListener("mouseup", () => isMouseDown = false);
+
+let color = "black"; //initial color
 container.addEventListener("mouseover", (e) => {
     if (isMouseDown) e.target.style.background = color
 });
-container.addEventListener("mousedown", (e) => e.target.style.background = color);
+container.addEventListener("mousedown", (e) => e.target.style.background = color); //ensures the square which was clicked first is also colored
 
+//Change color with palette
 const inputColor = document.querySelector("#inputColor");
 inputColor.addEventListener("mouseleave", () => {
     let boxes = document.querySelectorAll(".box");
@@ -57,36 +61,98 @@ inputColor.addEventListener("mouseleave", () => {
         box.addEventListener("mouseover",() => color = inputColor.value)
     });
     isRainbowActive = false;
+    isGreyScaleActive = false;
     eraser.classList.remove("btn-on");
     rainbowColor.classList.remove("btn-on");
+    greyScale.classList.remove("btn-on");
 });
 
+//Rainbow mode
 const rainbowColor = document.querySelector("#rainbow");
 function rainbowMode() {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
         box.addEventListener("mouseover",() => color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`)
     });
+    isGreyScaleActive = false;
     isRainbowActive = true;
     eraser.classList.remove("btn-on");
+    greyScale.classList.remove("btn-on");
     rainbowColor.classList.add("btn-on");
 }
 rainbowColor.addEventListener("click", rainbowMode);
 
+//Grey scale mode
+const greyScale = document.querySelector("#greyScale");
+function greyScaleMode() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover",() => {
+        if (box.classList.contains("grey10")) {
+            color = "rgb(0,0,0)";
+        } else if (box.classList.contains("grey9")) {
+            box.classList.remove("grey9");
+            box.classList.add("grey10");
+            color = "rgb(0,0,0)";
+        } else if (box.classList.contains("grey8")) {
+            box.classList.remove("grey8");
+            box.classList.add("grey9");  
+            color = "rgb(20,20,20)";
+        } else if (box.classList.contains("grey7")) {
+            box.classList.remove("grey7");
+            box.classList.add("grey8");
+            color = "rgb(40,40,40)";
+        } else if (box.classList.contains("grey6")) {
+            box.classList.remove("grey6");
+            box.classList.add("grey7");
+            color = "rgb(60,60,60)";
+        } else if (box.classList.contains("grey5")) {
+            box.classList.remove("grey5");
+            box.classList.add("grey6");
+            color = "rgb(80,80,80)";
+        } else if (box.classList.contains("grey4")) {
+            box.classList.remove("grey4");
+            box.classList.add("grey5");
+            color = "rgb(110,110,110)";
+        } else if (box.classList.contains("grey3")) {
+            box.classList.remove("grey3");
+            box.classList.add("grey4"); 
+            color = "rgb(140,140,140)";
+        } else if (box.classList.contains("grey2")) {
+            box.classList.remove("grey2");
+            box.classList.add("grey3");
+            color = "rgb(170,170,170)";
+        } else if (box.classList.contains("grey1")) {
+            box.classList.remove("grey1");
+            box.classList.add("grey2");
+            color = "rgb(200,200,200)";
+        } else {
+            box.classList.add("grey1");
+            color = "rgb(230,230,230)";
+            }
+        })
+    });
+    isGreyScaleActive = true;
+    isRainbowActive = false;
+    eraser.classList.remove("btn-on");
+    rainbowColor.classList.remove("btn-on");
+    greyScale.classList.add("btn-on");
+}
+greyScale.addEventListener("click", greyScaleMode);
+
+//Eraser
 const eraser = document.querySelector("#eraser");
 eraser.addEventListener("click", () => {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
         box.addEventListener("mouseover",() => color = "white");
     });
+    isGreyScaleActive = false;
     isRainbowActive = false;
     rainbowColor.classList.remove("btn-on");
+    greyScale.classList.remove("btn-on");
     eraser.classList.add("btn-on")
 });
-
-function clearGrid() {
-    container.innerHTML = "";
-}
 
 // NO LONGER NEEDED //
 
