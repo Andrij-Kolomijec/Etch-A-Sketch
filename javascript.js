b2747a29f2e84b1f534817b7b4ec93c48e849297
Difficins <a.kolomijec@seznam.cz>
2023-06-07 20:51:48 +0200

    Added removeEventListener for buttons

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ let squares = 16;
 let isRainbowActive = false;
 let isGreyScaleActive = false;
 let isMouseDown = false;
-let isSliderActive = false;
 
 function generateGrid(number) {
     let boxSize = `${480 / squares}px`;
@@ -19,10 +18,9 @@ function generateGrid(number) {
             row.appendChild(box);
         };
         container.appendChild(row);
-    if (isRainbowActive) rainbowMode();
     if (isGreyScaleActive) greyScaleMode();
-    // let boxes = document.querySelectorAll(".box");
-    };
+    if (isRainbowActive) rainbowMode();
+    }
 }
 
 generateGrid(squares);
@@ -31,14 +29,20 @@ generateGrid(squares);
 const slider = document.querySelector(".slider");
 const label = document.querySelector("label");
 slider.addEventListener("mousemove", () => {
+    if (isMouseDown) {
         squares = slider.value;
         label.textContent = `${squares}x${squares} squares`;
+        container.innerHTML = "";
+        generateGrid(squares);
+    }
 })
-slider.addEventListener("mouseup", () => {
+slider.addEventListener("click", () => {
+    squares = slider.value;
+    label.textContent = `${squares}x${squares} squares`;
     container.innerHTML = "";
     generateGrid(squares);
-    isSliderActive = false;
-})
+    }        
+)
 
 // Mouse down + hover, mouse up events
 document.addEventListener("mousedown", () => isMouseDown = true);
@@ -48,7 +52,7 @@ let color = "black"; //initial color
 function mouseEvents () {
     container.addEventListener("mouseover", (e) => {
         if (isMouseDown) e.target.style.background = color //to color one needs to press and hold the mouse
-        });
+        })
     container.addEventListener("mousedown", (e) => e.target.style.background = color); //ensures the square which was clicked first is also colored
 }
 
@@ -56,11 +60,12 @@ mouseEvents();
 
 //Change color with palette
 const inputColor = document.querySelector("#inputColor");
-inputColor.addEventListener("mouseleave", () => {
+inputColor.addEventListener("click", () => {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
+        box.removeEventListener("mouseover", boxHover);
         box.addEventListener("mouseover",() => color = inputColor.value)
-    });
+    })
     isRainbowActive = false;
     isGreyScaleActive = false;
     eraser.classList.remove("btn-on");
@@ -73,6 +78,7 @@ const rainbowColor = document.querySelector("#rainbow");
 function rainbowMode() {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
+        box.removeEventListener("mouseover", boxHover);
         box.addEventListener("mouseover",() => color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`)
     });
     isGreyScaleActive = false;
@@ -88,9 +94,10 @@ const greyScale = document.querySelector("#greyScale");
 function greyScaleMode() {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
+        box.removeEventListener("mouseover", boxHover);
         box.numPasses = 1;
         box.addEventListener("mouseover", boxHover)
-    });
+    })
     isGreyScaleActive = true;
     isRainbowActive = false;
     eraser.classList.remove("btn-on");
@@ -98,9 +105,11 @@ function greyScaleMode() {
     greyScale.classList.add("btn-on");
 }
 function boxHover(event) {
-    const box = event.target;
-    color= `rgba(0, 0, 0, ${(10 * box.numPasses) / 100})`;
-    box.numPasses++;
+    if (isMouseDown) {
+        const box = event.target;
+        color= `rgba(0, 0, 0, ${(10 * box.numPasses) / 100})`;
+        box.numPasses++;
+    }
 }
 greyScale.addEventListener("click", greyScaleMode);
 
@@ -108,12 +117,10 @@ greyScale.addEventListener("click", greyScaleMode);
 const eraser = document.querySelector("#eraser");
 eraser.addEventListener("click", () => {
     let boxes = document.querySelectorAll(".box");
-    boxes.forEach((box) => {
-        box.addEventListener("mouseover",() => color = "white");
-    });
+    boxes.forEach((box) => box.addEventListener("mouseover",() => color = "white"));
     isGreyScaleActive = false;
     isRainbowActive = false;
     rainbowColor.classList.remove("btn-on");
     greyScale.classList.remove("btn-on");
-    eraser.classList.add("btn-on")
+    eraser.classList.add("btn-on");
 })
